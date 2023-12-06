cf5a96a05617c655ed70062219cbce41ca10e313
Harwinder Singh <112082796+HarwinderSingh15@users.noreply.github.com>
2023-12-06 12:01:17 +0530

    Create ffmpegCompression.js

---
+++ ffmpegCompression.js
@@ -0,0 +1,21 @@
+const compressWithFFMpeg = async (
+  imagePath,
+  fileName,
+  progressCb,
+  successCb
+) => {
+  const currentFileName = fileName;
+  // First FFmpeg command
+  const documentDir = RNFetchBlob.fs.dirs.DocumentDir;
+  FFmpegKit.executeAsync(
+    `-i ${imagePath} -c:v libx264 -crf 30 -preset faster -r 30 -b:v 2M -movflags +faststart ${documentDir}/${currentFileName}.mp4`,
+    session => {
+      successCb(true, currentFileName);
+    },
+    logs => {},
+    stats => {
+      const statsTime = stats.getTime();
+      progressCb(statsTime);
+    }
+  );
+};
